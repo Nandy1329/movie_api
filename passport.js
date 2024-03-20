4bc40126c94f16438a5fb79bb40629cf46821b3d
Nicholas <nickis1329@gmail.com>
2024-03-19 19:43:45 -0700

    fixed Username in passport.js

--- passport.js
+++ passport.js
@@ -9,7 +9,11 @@ passport.use(new LocalStrategy({
   usernameField: 'username',    // define the parameter in req.body that passport can use as username and password
   passwordField: 'password'
 }, (username, password, done) => {
+<<<<<<< Updated upstream
   User.findOne({ username: username }, (err, user) => {
+=======
+  User.findOne({ Username: username }, (err, user) => {
+>>>>>>> Stashed changes
     if (err) { 
       return done(err);
     }
