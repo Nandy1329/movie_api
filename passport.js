7c5659901f1228a56866528bf5b31c795e8a3956
Nicholas <nickis1329@gmail.com>
2024-05-17 16:12:43 -0700

    rebased files

--- passport.js
+++ passport.js
@@ -1,44 +1,38 @@
-const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Models = require('./models.js');
+const passportJWT = require('passport-jwt');
 
-let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+const Users = Models.User;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password'
 }, (username, password, callback) => {
-  console.log(username + '  ' + password);
   Users.findOne({ Username: username })
-  .then( user => 
-    {
-
-      if (!user.validatePassword(password)) {
-    console.log('incorrect password');
-    return callback(null, false, {message: 'Incorrect Password'});
-  }
-    
-    return callback(null, user)
-  })
-
-  // eslint-disable-next-line no-unused-vars
-  .catch(e => callback(null, false, {message: 'Incorrect username or password.'}));
-  
-
+    .then(user => {
+      if (!user || !user.validatePassword(password)) {
+        return callback(null, false, { message: 'Incorrect username or password.' });
+      }
+      return callback(null, user);
+    })
+    .catch(error => callback(error));
 }));
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
-  return Users.findById(jwtPayload._id)
-    .then((user) => {
+  Users.findById(jwtPayload._id)
+    .then(user => {
+      if (!user) {
+        return callback(null, false);
+      }
       return callback(null, user);
     })
-    .catch((error) => {
-      return callback(error)
-    });
+    .catch(error => callback(error));
 }));
+
+module.exports = passport;
