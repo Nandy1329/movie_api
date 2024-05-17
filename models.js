f1d0cfab52d5dce061b73c2792e6afbad43cab6c
Nicholas <nickis1329@gmail.com>
2024-05-17 12:49:09 -0700

    redoing server connection

--- models.js
+++ models.js
@@ -19,20 +19,20 @@ let movieSchema = mongoose.Schema({
 const Movie = mongoose.model('Movie', movieSchema);
 
 let userSchema = mongoose.Schema({
-    Username: { type: String, required: true },
-    Password: { type: String, required: true },
-    Email: { type: String, required: true },
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
     Birthday: Date,
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-});
-
-userSchema.statics.hashPassword = (password) => {
+  });
+  
+  userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
-};
-
-userSchema.methods.validatePassword = function (password) {
+  };
+  
+  userSchema.methods.validatePassword = function(password) {
     return bcrypt.compareSync(password, this.Password);
-};
+  };
 
 const User = mongoose.model('User', userSchema);
 
